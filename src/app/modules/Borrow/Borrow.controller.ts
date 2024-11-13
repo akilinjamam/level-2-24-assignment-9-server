a5d17ec3b25ca42cc5ccfb57723f4957c502d21f
akilinjamam <akilinjamam@gmail.com>
2024-11-13 11:07:25 +0600

    file-updated

--- src/app/modules/Borrow/Borrow.controller.ts
+++ src/app/modules/Borrow/Borrow.controller.ts
@@ -4,7 +4,7 @@ import { borrowService } from "./Borrow.service";
 const createBorrow = tryCatchAsync(async (req, res) => {
   const result = await borrowService.createBorrow(req.body);
 
-  res.status(201).json({
+  res.status(200).json({
     success: true,
     status: 200,
     message: "Book borrowed successfully",
